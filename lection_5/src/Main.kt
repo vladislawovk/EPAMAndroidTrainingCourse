be7e0ac505c28b6036321fe8bdf2d689392ee1c6
vladislawovk <vladislawovk@gmail.com>
2021-05-21 15:47:59 +0300

    Homework

---
+++ lection_5/src/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val car = Car(color = Color.RED, carModel = "modelCar")
+    val bike = Bike(color = Color.BLUE, bikeModel = "modelBike")
+
+    val carGarage = Garage<Car>()
+    carGarage.addTransport(car)
+    println(carGarage.getTransport())
+
+    val bikeGarage = Garage<Bike>()
+    bikeGarage.addTransport(bike)
+    println(bikeGarage.getTransport())
+    bikeGarage.removeTransport(bike)
+    println(bikeGarage.getTransport())
+
+    val carUiModel = carGarage.getTransport().filter { it.color == Color.RED }.map { it.toTransportUiModel() }
+}
---
+++ lection_5/src/TransportUiModel.kt
@@ -0,0 +1,1 @@
+data class TransportUiModel(val color: Color)
